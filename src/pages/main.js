7d3c778289eeaad38b0ec6e847fb96af4283a025
rezasalehi <rezasalehidev@gmail.com>
2021-11-09 23:47:47 +0330

    add dots to project and use Animated interpolate scrollX and Divide

--- src/pages/main.js
+++ src/pages/main.js
@@ -36,6 +36,18 @@ const onBoardings = [
 const Main = () => {
   const scrollX = new Animated.Value(0);
 
+  const [completed, setCompleted] = React.useState(false);
+
+  React.useEffect(() => {
+    scrollX.addListener(({value}) => {
+      if (Math.floor(value / SIZES.width) === onBoardings.length - 1) {
+        setCompleted(true);
+      }
+    });
+
+    return () => scrollX.removeListener();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Animated.ScrollView
@@ -82,6 +94,38 @@ const Main = () => {
           );
         })}
       </Animated.ScrollView>
+
+      {onBoardings.map((item, index) => {
+        const dotPosition = Animated.divide(scrollX, SIZES.width);
+
+        return (
+          <View style={styles.dotsRootContainer}>
+            <View style={styles.dotsContainer}>
+              {onBoardings.map((item, index) => {
+                const opacity = dotPosition.interpolate({
+                  inputRange: [index - 1, index, index + 1],
+                  outputRange: [0.3, 1, 0.3],
+                  extrapolate: 'clamp',
+                });
+
+                const dotSize = dotPosition.interpolate({
+                  inputRange: [index - 1, index, index + 1],
+                  outputRange: [SIZES.base, 17, SIZES.base],
+                  extrapolate: 'clamp',
+                });
+
+                return (
+                  <Animated.View
+                    key={index}
+                    opacity={opacity}
+                    style={[styles.dot, {width: dotSize, height: dotSize}]}
+                  />
+                );
+              })}
+            </View>
+          </View>
+        );
+      })}
     </View>
   );
 };
@@ -89,6 +133,26 @@ const Main = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  dotsRootContainer: {
+    position: 'absolute',
+    justifyContent: 'center',
+    bottom: SIZES.height > 700 ? '20%' : '-10%',
+    left: '40%',
+    alignItems: 'center',
+  },
+  dotsContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: SIZES.padding / 2,
+    marginBottom: SIZES.padding * 3,
+    height: SIZES.padding,
+  },
+  dot: {
+    borderRadius: SIZES.radius,
+    backgroundColor: COLORS.blue,
+    marginHorizontal: SIZES.radius / 2,
   },
 });
 
